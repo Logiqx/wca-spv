9db785c3d7ea4d023d2d6be3560b86189c1bf746
Michael George <michael.george@directlinegroup.co.uk>
2016-08-24 08:27:14 +0100

    Query showing “average” solver for each country
    Ignore top and bottom 5% of competitors.
    
    Remove duplicated code for 444 and 555

--- query/fastest_333.sql
+++ query/fastest_333.sql
@@ -6,12 +6,11 @@ WHERE       bestAverage333 > 0
 GROUP BY    continentName, countryName
 ORDER BY    bestAverage333 ASC;
 
--- Countries ranked by their median 3x3 solver, restricted to countries with 10 or more persons
+-- Countries ranked by their "average" 3x3 solver (i.e. ignoring top and bottom 5%), restricted to countries with 10 or more persons
 SELECT      p.continentName, c.countryName, c.maxRank AS numPersons,
             TRUNCATE(AVG(bestSingle333) / 100, 2) AS medianSingle333, TRUNCATE(AVG(bestAverage333) / 100, 2) AS medianAverage333
 FROM        PersonsView p
-JOIN        RanksAverageCountryRollup c ON c.eventId = '333' AND c.countryName = p.countryName
-WHERE       countryCentileAverage333 = 50
-AND         c.maxRank > 9
+JOIN        RanksAverageCountryRollup c ON c.eventId = '333' AND c.countryName = p.countryName AND maxRank >= 10
+WHERE       countryCentileAverage333 BETWEEN 6 AND 95
 GROUP BY    continentName, countryName
 ORDER BY    medianAverage333 ASC;
