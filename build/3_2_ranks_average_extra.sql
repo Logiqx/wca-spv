05260f6823f730750eef50d5e191efb99e6ff31b
Logiqx <software@mikeg.me.uk>
2018-10-08 18:46:18 +0100

    Avoid issue with query optimiser in MySQL 5.5

--- build/3_2_ranks_average_extra.sql
+++ build/3_2_ranks_average_extra.sql
@@ -1,3 +1,39 @@
+-- Drop the existing ranks temporary table
+DROP TEMPORARY TABLE IF EXISTS RanksAverageTemp;
+
+-- Create the ranks temporary table to include centiles
+CREATE TEMPORARY TABLE RanksAverageTemp
+(
+    personId VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
+    eventId VARCHAR(6) COLLATE utf8_unicode_ci NOT NULL,
+    best INT DEFAULT 0 NOT NULL,
+    worldRank INT DEFAULT 0 NOT NULL,
+    continentRank INT DEFAULT 0 NOT NULL,
+    countryRank INT DEFAULT 0 NOT NULL,
+	continentName VARCHAR(50),
+	countryName VARCHAR(50),
+    PRIMARY KEY (personId, eventId)
+)
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Populate the ranks temporary table
+-- Note: "RIGHT JOIN" and "WHERE" have been used because a regular "INNER JOIN" never seems to complete on MySQL 5.5
+INSERT INTO RanksAverageTemp (personId, eventId, best, worldRank, continentRank, countryRank, continentName, countryName)
+SELECT      r.personId,
+            r.eventId,
+            r.best,
+            r.worldRank,
+            r.continentRank,
+            r.countryRank,
+            p.continentName,
+            p.countryName
+FROM        RanksAverage r
+RIGHT JOIN  PersonsExtra p ON p.id = r.personId
+WHERE       r.personid IS NOT NULL;
+
+-- Update statistics
+ANALYZE TABLE RanksAverageTemp;
+
 -- Drop the existing ranks extra table
 DROP TABLE IF EXISTS RanksAverageExtra;
 
@@ -29,11 +65,10 @@ SELECT      r.personId,
             CEILING(100.0 * r.worldRank / r1.maxRank) AS worldCentile,
             CEILING(100.0 * r.continentRank / r2.maxRank) AS continentCentile,
             CEILING(100.0 * r.countryRank / r3.maxRank) AS countryCentile
-FROM        RanksAverage r
-INNER JOIN  PersonsExtra p ON p.id = r.personId
-INNER JOIN  RanksAverageWorldRollup r1 ON r1.eventId = r.eventId
-INNER JOIN  RanksAverageContinentRollup r2 ON r2.eventId = r.eventId AND r2.continentName = p.continentName
-INNER JOIN  RanksAverageCountryRollup r3 ON r3.eventId = r.eventId AND r3.countryName = p.countryName;
+FROM        RanksAverageTemp r
+INNER JOIN  RanksAverageWorldRollup r1 ON r.eventId = r1.eventId
+INNER JOIN  RanksAverageContinentRollup r2 ON r2.eventId = r.eventId AND r2.continentName = r.continentName
+INNER JOIN  RanksAverageCountryRollup r3 ON r3.eventId = r.eventId AND r3.countryName = r.countryName;
 
 -- Update statistics
 ANALYZE TABLE RanksAverageExtra;
--- build/3_2_ranks_single_extra.sql
+++ build/3_2_ranks_single_extra.sql
@@ -1,3 +1,39 @@
+-- Drop the existing ranks temporary table
+DROP TEMPORARY TABLE IF EXISTS RanksSingleTemp;
+
+-- Create the ranks temporary table to include centiles
+CREATE TEMPORARY TABLE RanksSingleTemp
+(
+    personId VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
+    eventId VARCHAR(6) COLLATE utf8_unicode_ci NOT NULL,
+    best INT DEFAULT 0 NOT NULL,
+    worldRank INT DEFAULT 0 NOT NULL,
+    continentRank INT DEFAULT 0 NOT NULL,
+    countryRank INT DEFAULT 0 NOT NULL,
+	continentName VARCHAR(50),
+	countryName VARCHAR(50),
+    PRIMARY KEY (personId, eventId)
+)
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Populate the ranks temporary table
+-- Note: "RIGHT JOIN" and "WHERE" have been used because a regular "INNER JOIN" never seems to complete on MySQL 5.5
+INSERT INTO RanksSingleTemp (personId, eventId, best, worldRank, continentRank, countryRank, continentName, countryName)
+SELECT      r.personId,
+            r.eventId,
+            r.best,
+            r.worldRank,
+            r.continentRank,
+            r.countryRank,
+            p.continentName,
+            p.countryName
+FROM        RanksSingle r
+RIGHT JOIN  PersonsExtra p ON p.id = r.personId
+WHERE       r.personid IS NOT NULL;
+
+-- Update statistics
+ANALYZE TABLE RanksSingleTemp;
+
 -- Drop the existing ranks extra table
 DROP TABLE IF EXISTS RanksSingleExtra;
 
@@ -29,11 +65,10 @@ SELECT      r.personId,
             CEILING(100.0 * r.worldRank / r1.maxRank) AS worldCentile,
             CEILING(100.0 * r.continentRank / r2.maxRank) AS continentCentile,
             CEILING(100.0 * r.countryRank / r3.maxRank) AS countryCentile
-FROM        RanksSingle r
-INNER JOIN  PersonsExtra p ON p.id = r.personId
-INNER JOIN  RanksSingleWorldRollup r1 ON r1.eventId = r.eventId
-INNER JOIN  RanksSingleContinentRollup r2 ON r2.eventId = r.eventId AND r2.continentName = p.continentName
-INNER JOIN  RanksSingleCountryRollup r3 ON r3.eventId = r.eventId AND r3.countryName = p.countryName;
+FROM        RanksSingleTemp r
+INNER JOIN  RanksSingleWorldRollup r1 ON r.eventId = r1.eventId
+INNER JOIN  RanksSingleContinentRollup r2 ON r2.eventId = r.eventId AND r2.continentName = r.continentName
+INNER JOIN  RanksSingleCountryRollup r3 ON r3.eventId = r.eventId AND r3.countryName = r.countryName;
 
 -- Update statistics
 ANALYZE TABLE RanksSingleExtra;
