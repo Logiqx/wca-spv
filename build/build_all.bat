fc1db683358b96a29ab36a935c577481cf5088b3
Logiqx <software@mikeg.me.uk>
2019-03-23 18:52:15 +0000

    Allow remote database for Docker

--- build/build_all.bat
+++ build/build_all.bat
@@ -1,5 +1,6 @@
+SET MYSQL_HOST=mysql
 SET MYSQL_DATABASE=wca
 SET MYSQL_USERNAME=root
 
-mysql --verbose --database=%MYSQL_DATABASE% --user=%MYSQL_USERNAME% --password --execute="source build_all.sql"
+mysql --verbose --host=%MYSQL_HOST% --database=%MYSQL_DATABASE% --user=%MYSQL_USERNAME% --password --execute="source build_all.sql"
 
--- build/build_all.sh
+++ build/build_all.sh
@@ -1,5 +1,6 @@
+MYSQL_HOST=mysql
 MYSQL_DATABASE=wca
 MYSQL_USERNAME=root
 
-mysql --verbose --database=$MYSQL_DATABASE --user=$MYSQL_USERNAME --password --execute="source build_all.sql"
+mysql --verbose --host=$MYSQL_HOST --database=$MYSQL_DATABASE --user=$MYSQL_USERNAME --password --execute="source build_all.sql"
 
